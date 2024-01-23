8779d551c0cc5c6b18eb5541dcddd5011b811a0f
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2024-01-22 21:20:54 -0300

    MVP pronto

---
+++ index.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+numero_misterioso = "7"
+
+resposta_usuario = input("Escolha um número de 0 a 10")
+
+if resposta_usuario == numero_misterioso:
+  print("Parabéns, você ganhou!!!!")
+else:
+  print("Que pena, você perdeu =(")
